e766ba83e2adf76669e48c21a47039ed942187f3
HJay <exist91@gmail.com>
2015-01-03 12:34:14 +0900

    계산기 최초 저장

---
+++ src/Calculater.java
@@ -0,0 +1,24 @@
+
+public class Calculater {
+
+	int MyAdd(int operand1, int operand2)
+	{
+		return operand1 + operand2;
+	}
+	
+	int MySub(int operand1, int operand2)
+	{
+		return operand1 - operand2;
+	}
+	
+	int MyMult(int operand1, int operand2)
+	{
+		return operand1 * operand2;
+	}
+	
+	float MyDiv(float operand1, float operand2)
+	{
+		return operand1 / operand2;
+	}
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main 
+{
+	public static void main(String[] args)
+	{	
+		Calculater calc = new Calculater();
+
+		System.out.println( calc.MyAdd(20, 10) );
+		System.out.println( calc.MySub(20, 10) );
+		System.out.println( calc.MyMult(20, 10) );
+		System.out.println( calc.MyDiv(20, 10) );
+		
+	}
+}
